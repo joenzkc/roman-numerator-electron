830ab854502a3487250a6e391d008c2fb7a4ca47
joenzkc <jokichaa@gmail.com>
2023-12-19 20:30:43 +0800

    handle numbers

--- renderer/components/Converter.tsx
+++ renderer/components/Converter.tsx
@@ -18,16 +18,35 @@ const Converter = () => {
     }
   };
 
+  const handleChangeNumeric = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setNumericValue(event.target.value);
+    // check if valid number
+    if (!isNaN(Number(event.target.value))) {
+      setNumericError(false);
+    } else {
+      setNumericError(true);
+    }
+  };
+
   return (
-    <Flex>
+    <Flex className=" space-x-2 p-2">
       <Input
         placeholder="Roman"
         errorBorderColor="red.300"
         onChange={(e) => handleChangeRoman(e)}
+        value={romanValue}
         focusBorderColor={romanError ? "red.300" : "blue.300"}
         isInvalid={romanError}
       ></Input>
-      <Input placeholder="Numerics"></Input>
+      <Input
+        placeholder="Number"
+        errorBorderColor="red.300"
+        value={numericValue}
+        onChange={handleChangeNumeric}
+        focusBorderColor={numericError ? "red.300" : "blue.300"}
+        isInvalid={numericError}
+      ></Input>
     </Flex>
   );
 };
