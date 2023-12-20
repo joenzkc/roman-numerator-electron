76efd9abff6449f787896831c6190df195722f0e
Joenz Kim-Chan <jokichaa@gmail.com>
2023-12-20 14:40:13 +0800

    add dummy data

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import RomanToNumeralConverterContainer from "../components/RomanToNumeral/RomanToNumeralConverterContainer";
 import HomeContainer from "../components/HomeContainer";
 import PhonebookContainer from "../components/Phonebook/PhonebookContainer";
+import { DummyContacts } from "../utils/DummyContacts";
 
 export default function HomePage() {
   const [app, setApp] = React.useState("romanToNumeral");
 
-  const [contacts, setContacts] = React.useState([]);
+  const [contacts, setContacts] = React.useState(DummyContacts);
+
+  useEffect(() => {
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
+    console.log(contacts);
+  }, [contacts]);
 
   return (
     <React.Fragment>
---
+++ renderer/utils/DummyContacts.ts
@@ -0,0 +1,35 @@
+import { Contact } from "../components/Phonebook/PhonebookContainer";
+
+// examples with dummy data with multiple starting letters for name
+export const DummyContacts: Contact[] = [
+  {
+    name: "John Doe",
+    number: "1234567890",
+    address: "1234 Main St",
+  },
+  {
+    name: "Jane Doe",
+    number: "0987654321",
+    address: "4321 Main St",
+  },
+  {
+    name: "John Smith",
+    number: "1234567890",
+    address: "1234 Main St",
+  },
+  {
+    name: "Alex Smith",
+    number: "0987654321",
+    address: "4321 Main St",
+  },
+  {
+    name: "Bob Smith",
+    number: "1234567890",
+    address: "1234 Main St",
+  },
+  {
+    name: "Carl Smith",
+    number: "0987654321",
+    address: "4321 Main St",
+  },
+];
